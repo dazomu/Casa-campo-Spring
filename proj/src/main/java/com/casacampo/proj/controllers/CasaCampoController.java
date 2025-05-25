71d081646f47ca4a4117a8e3d528a364a25c6751
Oswaldo Perez <oeperezbellido@gmail.com>
2025-05-25 12:25:08 -0500

    crud basico completo

--- proj/src/main/java/com/casacampo/proj/controllers/CasaCampoController.java
+++ proj/src/main/java/com/casacampo/proj/controllers/CasaCampoController.java
@@ -18,6 +18,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.casacampo.proj.entities.CasaCampo;
 import com.casacampo.proj.repositories.CasaCampoRepository;
 import com.casacampo.proj.services.CasaCampoService;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -59,37 +61,54 @@ public class CasaCampoController {
     @PostMapping("/editar")
     public String procesarFormEditar(
         @ModelAttribute("casa") CasaCampo casa,
-        @RequestParam("imagenUrl") String imagenUrl,
         RedirectAttributes redirectAttributes) {
 
         try {
-            if (imagenUrl != null && !imagenUrl.trim().isEmpty()) {
-                casa.setImagen(imagenUrl.trim());
-            } else {
-                CasaCampo casaExistente = casaCampoService.findById(casa.getId_casa());
-                if (casaExistente != null) {
-                    casa.setImagen(casaExistente.getImagen());
-                }
-            }
-
+            // Guarda normalmente
             casaCampoService.save(casa);
             redirectAttributes.addFlashAttribute("mensaje", "Casa actualizada correctamente");
+
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("error", "Error al actualizar la casa: " + e.getMessage());
             e.printStackTrace();
         }
 
-        return "redirect:/casas/lista";
-    }
+        return "redirect:/casas";
 
-    @GetMapping("/nuevaCasa")
-    public String nuevaCasa() {
-        
+        }
+
+    @GetMapping("/nueva_casa")
+    public String mostrarFormCasa(Model model) {
+        model.addAttribute("casa", new CasaCampo()); 
         return "nuevaCasa";
     }
     
-    
+    @PostMapping("/nueva_casa")
+    public String procesarFormCasa(@ModelAttribute CasaCampo casa, RedirectAttributes redirectAttributes) {
 
+        try {
+       
+        casaCampoService.save(casa);
+
+            redirectAttributes.addFlashAttribute("mensaje", "Casa agregada correctamente");
+        
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Error al agregar casa: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+
+        return "redirect:/casas";
+    }
+    
+    @PostMapping("/eliminar/{idCasa}")
+    public String eliminarCasa(@PathVariable Long idCasa) {
+        
+        casaCampoService.delete(idCasa);
+        
+        return "redirect:/casas";
+    }
+    
 
 
 }
